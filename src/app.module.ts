d1cf009d457804c657fca035318d030a801c8e9d
kkpagaev <kkpagaev@gmail.com>
2023-04-17 02:50:29 +0300

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { DbModule } from "./db/db.module"
 import { UserModule } from "./user/user.module"
+import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [DbModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env", ".env." + process.env.NODE_ENV],
+    }),
+    DbModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,19 +1,25 @@
 import { Module } from "@nestjs/common"
 import { PG_CONNECTION } from "../constants"
 import { Pool } from "pg"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 
 const dbProvider = {
   provide: PG_CONNECTION,
-  useValue: new Pool({
-    user: "user",
-    host: "127.0.0.1",
-    database: "user",
-    password: "user",
-    port: 5438,
-  }),
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (conf: ConfigService) => {
+    return new Pool({
+      user: conf.get("DB_USER"),
+      host: conf.get("DB_HOST"),
+      database: conf.get("DB_NAME"),
+      password: conf.get("DB_PWD"),
+      port: conf.get("DB_PORT"),
+    })
+  },
 }
 
 @Module({
+  imports: [ConfigModule],
   exports: [dbProvider],
   providers: [dbProvider],
 })
