03408504cfb679c2e076fcde890156492fba1083
kkpagaev <kkpagaev@gmail.com>
2023-04-17 02:56:06 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { swagger } from "./swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  if (process.env.NODE_ENV === "dev") {
+    swagger(app)
+  }
+
   await app.listen(3000)
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
