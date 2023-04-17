504e98c5703a576aff286bb8226bf7dcd095508e
kkpagaev <kkpagaev@gmail.com>
2023-04-17 04:20:52 +0300

    add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,22 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { JwtModule } from "@nestjs/jwt"
 import { UserModule } from "../user/user.module"
 import { AuthController } from "./auth.controller"
 import { AuthService } from "./auth.service"
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get("JWT_SECRET"),
+        signOptions: { expiresIn: "1d" },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,19 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common"
 import { UserService } from "../user/user.service"
 import { compareSync } from "bcrypt"
+import { JwtService } from "@nestjs/jwt"
+
+interface JwtPayload {
+  user_id: number
+  email: string
+}
 
 @Injectable()
 export class AuthService {
-  constructor(private users: UserService) {}
+  constructor(
+    private users: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signIn(email: string, pass: string): Promise<any> {
     const user = await this.users.findByEmail(email)
@@ -12,6 +21,11 @@ export class AuthService {
       throw new UnauthorizedException("Invalid credentials")
     }
     // TODO: return a JWT
-    return user
+    const payload: JwtPayload = { user_id: user.id, email: user.email }
+    const accessToken = await this.jwtService.signAsync(payload)
+
+    return {
+      accessToken,
+    }
   }
 }
